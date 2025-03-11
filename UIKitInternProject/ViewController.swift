c8f95e646605af351d02bb50f79cac0668b8a7eb
Bálint Kozák <balint.kozak@capgemini.com>
2025-03-11 08:49:29 +0100

    Initial Commit

---
+++ UIKitInternProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitInternProject
+//
+//  Created by Balint Kozak on 2025. 03. 11..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
